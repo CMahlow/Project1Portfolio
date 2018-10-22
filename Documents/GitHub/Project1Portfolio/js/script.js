3c8bf301fafec48865bbec7d81fbda9d7c8b3cf3
Coding Mommy <christymahlow@gmail.com>
2018-10-22 13:05:47 -0700

    Remove git merge error message

--- Documents/GitHub/Project1Portfolio/js/script.js
+++ Documents/GitHub/Project1Portfolio/js/script.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 (function(){
 
   const MVC = function MVC() {
@@ -8,12 +7,3 @@
     self.name = ko.observable ('Designer Turned Web Developer');
   }
   ko.applyBinding(new MVC());
-
-})()
-=======
-const MVC = function
->>>>>>> 760cca034fe33b347dc12e4d4338c6aba4f1aa68
-||||||| merged common ancestors
-=======
-const MVC = function
->>>>>>> fcf07fc2ec8ae4f3de7cdeb0aac85f6066d1f03b
